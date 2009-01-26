1bfab1fe910924a82befd5c1b65b467f85db56da
Tyler Church <tylertrain@yahoo.com>
2009-01-25 20:27:54 -0700

    Added block image, blocks now populate the game window

--- totheexit.rb
+++ totheexit.rb
@@ -11,19 +11,25 @@ class GameWindow < Gosu::Window
 		@cursor = Gosu::Image.new self, "media/Cursor.png", false
 		@exit_size = @exit_image.width
 		@level = 1
+		@blocks = []
 		move_exit
 	end
 	
 	def update
 		self.caption = "x: #{self.mouse_x}, y: #{self.mouse_y}, level: #{@level}"
-		
+		@blocks.each {|block| block.update}
 		if mouse_x > @exit_x and mouse_x < @exit_x + @exit_size and mouse_y > @exit_y and mouse_y < @exit_y + @exit_size
 			@level += 1
+			@blocks = []
+			@level.times do |x|
+				@blocks += [Block.new self]
+			end
 			move_exit
 		end
 	end
 	
 	def draw
+		@blocks.each {|block| block.draw}
 		@cursor.draw mouse_x, mouse_y, ZOrder::Mouse
 		@exit_image.draw @exit_x, @exit_y, ZOrder::Objects
 	end
@@ -40,6 +46,26 @@ class GameWindow < Gosu::Window
 		if @exit_x < 0 or @exit_y < 0 or @exit_x + @exit_size > self.width or @exit_y + @exit_size > self.height
 			move_exit
 		end
+	end
+end
+
+class Block
+	def initialize window
+		@window = window
+		@image = Gosu::Image.new @window, 'media/block.png', true
+		@factor = 1
+		@x = rand(@window.width)
+		@y = rand(@window.height)
+	end
+	
+	def draw
+		@image.draw @x, @y, ZOrder::Objects, @factor, @factor
+	end
+	
+	def update
+		@factor += 0.01
+		@width = @image.width * @factor
+		@height = @image.height * @factor
 	end
 end
 
